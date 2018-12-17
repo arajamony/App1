90ad2d3962b7a9daba0a65bd71b44c8e3fd20724
Amaladoss <amaladossrajamony@gmail.com>
2018-12-17 08:06:04 +0530

    Implemented Recycler View and Card View_2

--- app/src/main/java/com/example/rocka/app1/HobbiesActivity.kt
+++ app/src/main/java/com/example/rocka/app1/HobbiesActivity.kt
@@ -1,17 +1,14 @@
 package com.example.rocka.app1
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_app1.*
 import kotlinx.android.synthetic.main.activity_hobbies.*
 
 class HobbiesActivity:AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hobbies)
 
         val layoutManager = LinearLayoutManager(this)
--- app/src/main/java/com/example/rocka/app1/HobbiesAdaptor.kt
+++ app/src/main/java/com/example/rocka/app1/HobbiesAdaptor.kt
@@ -1,10 +1,13 @@
 package com.example.rocka.app1
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_app1.*
 import kotlinx.android.synthetic.main.list_item.view.*
 import java.text.FieldPosition
 import kotlin.contracts.Returns
@@ -28,8 +31,30 @@ class HobbiesAdaptor(val context:Context,val hobbies:List<Hobby>):RecyclerView.A
 
     inner class myViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
+        var currentHobby:Hobby?=null
+        var currentPosition:Int =0
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context, currentHobby!!.title, Toast.LENGTH_SHORT).show()
+            }
+
+            itemView.imgShare.setOnClickListener {
+                val message: String = "My Hobby is " + currentHobby!!.title
+                val intent = Intent()
+
+                intent.action = Intent.ACTION_SEND
+
+                intent.putExtra(Intent.EXTRA_TEXT, message)
+                intent.type = "text/plain"
+                context.startActivity(Intent.createChooser(intent, "Share to: "))
+            }
+        }
+
         fun SetData(hobby:Hobby?,Pos:Int) {
             itemView.txvTitle.text = hobby!!.title
+            this.currentHobby = hobby
+            this.currentPosition = Pos
         }
     }
 }
